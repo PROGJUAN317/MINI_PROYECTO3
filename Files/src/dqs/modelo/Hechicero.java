5348594e0ff1e27bf700ae585133dbb8bfe1bdac
PROGJUAN371 <juan.sebastian.navarrete@correounivalle.edu.co>
2025-10-22 17:20:14 -0500

    Añade la interfaz Hechicero y métodos de hechizos en la clase Heroe; actualiza la clase Personaje para manejar estados de paralización y aumento de ataque.

---
+++ Files/src/dqs/modelo/Hechicero.java
@@ -0,0 +1,9 @@
+package dqs.modelo;
+
+public interface Hechicero {
+
+    void LanzaHechizoSueño(Personaje objetivo);
+    void LanzaHechizoRefuerzo(Personaje objetivo);
+    void LanzaHechizoParalisis(Personaje objetivo);
+
+}
